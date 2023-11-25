639640b9105b6ec7e0472559333c99866e142ed5
Erkinov Diyorbek <erkinovdiyorbek39@gmail.com>
2023-11-25 16:49:23 +0500

    some code 2

--- app/(root)/women/page.tsx
+++ app/(root)/women/page.tsx
@@ -9,6 +9,7 @@ const Women = () => {
       <Header />
       <main className="min-h-[90vh] mt-[10vh] px-12">
         <h1>Bu Women page figmadagi Women qismi bo'ladi</h1>
+        <h2>some code</h2>
       </main>
       <Footer />
     </>
