f3226d644610139f0ee3d98103e9d96d1b2806fc
Fayozbek-7073 <newfayozbek1@gmail.com>
2023-11-25 17:45:10 +0500

    codeew222222

--- app/(root)/women/page.tsx
+++ app/(root)/women/page.tsx
@@ -8,7 +8,7 @@ const Women = () => {
     <>
       <Header />
       <main className="min-h-[90vh] mt-[10vh] px-12">
-        <h1>Bu Women page figmadagi Women qismi bo'ladi</h1>
+        <h1>Bu Women page figmadagi Women qismi boladi</h1>
 
         <h2>Coodeeeeee</h2>
 
